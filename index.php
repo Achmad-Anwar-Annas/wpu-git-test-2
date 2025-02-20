9359bc4a837cdfbf000b7158f4f45e2d623d549d
Achmad Anwar Annas <achmadanwarannas@outlook.com>
2025-02-21 06:01:51 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Coba hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nostrum sapiente nemo labore esse, eligendi tenetur fugit voluptas optio ipsa repudiandae neque minus quibusdam. Consectetur, nihil optio? Molestias, fugit impedit?</p>
 </body>
 </html>
